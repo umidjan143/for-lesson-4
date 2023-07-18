64d587bb25df85f5fb9c8ad7b71f2ad47f793ef6
umidjan143 <xasanbayevumidjon@gmail.com>
2023-07-18 19:39:18 +0500

    lesson 4 homework

---
+++ main.js
@@ -0,0 +1,162 @@
+//1
+
+// let animals = new Map()
+
+// console.log(animals);
+
+// animals.set("cats", 10);
+// animals.set("dogs", 20);
+// animals.set("fish", 30);
+// animals.set("birds", 40);
+
+
+
+// animals.get(fish) ; 
+
+
+
+
+
+
+
+//2
+
+// let students = new Map()
+
+// console.log(students);
+
+// students.set("Muhammad", 20);
+// students.set("Shaxzoda", 21);
+// students.set("Gulhayo", 19);
+// students.set("Adxam", 21);
+
+
+// students.clear()
+
+
+
+
+
+
+
+//3
+
+// let cars = new Map()
+
+// console.log(cars);
+
+
+// cars.set("lambo", 350)
+// cars.set("BMW", 350)
+// cars.set("Mercedes", 330)
+// cars.set("Ferrari", 340)
+// cars.set("Chevrolet", 200)
+
+
+// cars.delete("lambo")
+
+
+
+//4
+
+
+// let cars = new Map()
+
+// console.log(cars);
+
+
+// cars.set("lambo", 350)
+// cars.set("BMW", 350)
+// cars.set("Mercedes", 330)
+// cars.set("Ferrari", 340)
+// cars.set("Chevrolet", 200)
+
+
+// cars.delete("BMW")
+
+
+
+
+
+//5
+
+// let cars = new Map()
+
+// console.log(cars);
+
+
+// cars.set("lambo", 350)
+// cars.set("BMW", 350)
+// cars.set("Mercedes", 330)
+// cars.set("Ferrari", 340)
+// cars.set("Chevrolet", 200)
+
+
+// cars.delete("Mercedes")
+
+
+
+
+//6
+
+// let cars = new Map()
+
+// console.log(cars);
+
+
+// cars.set("lambo", 350)
+// cars.set("BMW", 350)
+// cars.set("Mercedes", 330)
+// cars.set("Ferrari", 340)
+// cars.set("Chevrolet", 200)
+
+
+// cars.delete("Ferrari")
+
+
+
+//7
+
+
+// let cars = new Map()
+
+// console.log(cars);
+
+
+// cars.set("lambo", 350)
+// cars.set("BMW", 350)
+// cars.set("Mercedes", 330)
+// cars.set("Ferrari", 340)
+// cars.set("Chevrolet", 200)
+
+
+// cars.delete("Chevrolet")
+
+
+
+
+
+
+
+
+
+
+
+//10
+
+
+
+
+
+// let links = document.links ; 
+
+// for (let i = 0; i < links.length; i++) {
+// links[i].textContent = 'homework' ;
+    
+// }
+
+
+
+
+
+
